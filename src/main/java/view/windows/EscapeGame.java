282be446ff2ba7d151375bb4a8ed9cf51c4f6d3b
Kabix-dk <Kabix.dk@gmail.com>
2019-11-26 19:09:04 +0100

    Final game version.

--- src/main/java/view/windows/EscapeGame.java
+++ src/main/java/view/windows/EscapeGame.java
@@ -1,5 +1,6 @@
 package view.windows;
 
+import com.googlecode.lanterna.TextColor;
 import jdk.nashorn.internal.ir.Terminal;
 import view.SampleWindow;
 import com.googlecode.lanterna.TerminalSize;
@@ -32,8 +33,58 @@ public class EscapeGame extends SampleWindow {
         this.returnButton = new Button("POWRÓT");
         returnButton.setLayoutData(GridLayout.createLayoutData(GridLayout.Alignment.CENTER, GridLayout.Alignment.CENTER));
         panel.addComponent(returnButton);
+        panel.addComponent(new EmptySpace(new TerminalSize(0,1)));
 
         panel.addTo(mainPanel);
+
+        Panel helpPanel = new Panel();
+        helpPanel.setLayoutManager(new GridLayout(2));
+        helpPanel.addComponent(new Label("Stacja benzynowa:"));
+        Label label = new Label("  ");
+        label.setBackgroundColor(TextColor.ANSI.YELLOW);
+        helpPanel.addComponent(label);
+
+        helpPanel.addComponent(new Label("Agresywny klient:"));
+        Label label1 = new Label("! ");
+        label1.setForegroundColor(TextColor.ANSI.RED);
+        helpPanel.addComponent(label1);
+
+        helpPanel.addComponent(new Label("Anonimowy klient:"));
+        Label label2 = new Label("# ");
+        label2.setForegroundColor(TextColor.ANSI.CYAN);
+        helpPanel.addComponent(label2);
+
+        helpPanel.addComponent(new Label("Zwykły klient:"));
+        Label label3 = new Label("@ ");
+        label3.setForegroundColor(TextColor.ANSI.BLUE);
+        helpPanel.addComponent(label3);
+
+        helpPanel.addComponent(new Label("Bogaty klient:"));
+        Label label4 = new Label("$ ");
+        label4.setForegroundColor(TextColor.ANSI.GREEN);
+        helpPanel.addComponent(label4);
+
+        helpPanel.addComponent(new Label("Dom agresywnego klienta:"));
+        Label label5 = new Label("  ");
+        label5.setBackgroundColor(TextColor.ANSI.RED);
+        helpPanel.addComponent(label5);
+
+        helpPanel.addComponent(new Label("Dom anonimowego klienta:"));
+        Label label6 = new Label("  ");
+        label6.setBackgroundColor(TextColor.ANSI.CYAN);
+        helpPanel.addComponent(label6);
+
+        helpPanel.addComponent(new Label("Dom zwykłego klienta:"));
+        Label label7 = new Label("  ");
+        label7.setBackgroundColor(TextColor.ANSI.BLUE);
+        helpPanel.addComponent(label7);
+
+        helpPanel.addComponent(new Label("Dom bogatego klienta:"));
+        Label label8 = new Label("  ");
+        label8.setBackgroundColor(TextColor.ANSI.GREEN);
+        helpPanel.addComponent(label8);
+
+        helpPanel.addTo(mainPanel);
 
         super.setComponent(mainPanel);
     }
